0feee3d049780cb77035c4a39195995200e6e0c0
L1sa620 <lisevbom@gmail.com>
2025-11-12 01:48:41 +0300

    Big fix spiral(miss left and right)

--- matveev.borislav/P3/var6.cpp
+++ matveev.borislav/P3/var6.cpp
@@ -56,27 +56,33 @@ namespace matveev
     delete[] matrix;
   }
 void spiral(int** matrix, int rows, int cols)
-  {
-    int up_r = 0, left_c =0;
-    int down_r = rows - 1, right_c = cols - 1;
-    int counter = 1;
-    while (up_r <= down_r && left_c <= right_c) {
+{
+  int up_r = 0, left_c = 0;
+  int down_r = rows - 1, right_c = cols - 1;
+  int counter = 1;
+  while (up_r <= down_r && left_c <= right_c) {
+    for (int i = left_c; i <= right_c; ++i) {
+      matrix[down_r][i] += counter++;
+    }
+    down_r--;
+    if (left_c <= right_c) {
       for (int i = down_r; i >= up_r; --i) {
-        matrix[i][left_c] += counter++;
-      }
-      left_c++;
-      for (int i = left_c; i <= right_c; ++i) {
-        matrix[up_r][i] += counter++;
-      }
-      up_r++;
-      for (int i = up_r; i <= down_r; ++i) {
         matrix[i][right_c] += counter++;
       }
       right_c--;
+    }
+    if (up_r <= down_r) {
       for (int i = right_c; i >= left_c; --i) {
-        matrix[down_r][i] += counter++;
+        matrix[up_r][i] += counter++;
       }
-      down_r--;
+      up_r++;
+    }
+    if (left_c <= right_c) {
+      for (int i = up_r; i <= down_r; ++i) {
+        matrix[i][left_c] += counter++;
+      }
+      left_c++;
     }
   }
+}
 }
