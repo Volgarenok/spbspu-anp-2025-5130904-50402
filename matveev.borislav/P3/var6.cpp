636e28406d059801368eb0fd570407633aed8030
L1sa620 <lisevbom@gmail.com>
2025-11-12 01:15:23 +0300

    realize main function and fix wMatrix

--- matveev.borislav/P3/var6.cpp
+++ matveev.borislav/P3/var6.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 namespace matveev
 {
-  void rMatrix(int** matrix, int& rows, int& cols);
+  void rMatrix(int& rows, int& cols);
   void wMatrix(int** matrix, int rows, int cols);
   int** allocMatrix(int rows, int cols);
   void rm(int** matrix, int rows);
@@ -10,13 +10,18 @@ namespace matveev
 
 int main()
 {
-
+  int rows = 0, cols = 0;
+  matveev::rMatrix(rows, cols);
+  int** matrix = matveev::allocMatrix(rows, cols);
+  matveev::spiral(matrix, rows, cols);
+  matveev::wMatrix(matrix, rows, cols);
+  matveev::rm(matrix, rows);
 return 0;
 }
 
 namespace matveev
 {
-  void rMatrix(int** matrix, int& rows, int& cols)
+  void rMatrix(int& rows, int& cols)
   {
     std::cin >> rows >> cols;
   }
@@ -29,8 +34,8 @@ namespace matveev
         }
         std::cout << matrix[i][j];
       }
+      std::cout << "\n";
     }
-    std::cout << "\n";
   }
   int** allocMatrix(int rows, int cols)
   {
@@ -55,23 +60,19 @@ void spiral(int** matrix, int rows, int cols)
     int up_r = 0, left_c =0;
     int down_r = rows - 1, right_c = cols - 1;
     int counter = 1;
-
     while (up_r <= down_r && left_c <= right_c) {
       for (int i = down_r; i >= up_r; --i) {
         matrix[i][left_c] += counter++;
       }
       left_c++;
-
       for (int i = left_c; i <= right_c; ++i) {
         matrix[up_r][i] += counter++;
       }
       up_r++;
-
       for (int i = up_r; i <= down_r; ++i) {
         matrix[i][right_c] += counter++;
       }
       right_c--;
-
       for (int i = right_c; i >= left_c; --i) {
         matrix[down_r][i] += counter++;
       }
