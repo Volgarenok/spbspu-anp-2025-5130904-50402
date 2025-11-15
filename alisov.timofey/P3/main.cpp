6bcc093b3c6a81d3bf68d339641aec67265b3a50
AlisovTimofey <timofeyalisov@gmail.com>
2025-11-15 17:23:13 +0300

    Almost finish main function

--- alisov.timofey/P3/main.cpp
+++ alisov.timofey/P3/main.cpp
@@ -3,76 +3,90 @@
 #include <cstring>
 #include <limits>
 
-namespace alisov 
-{
-   
-}
 
 bool isdig(char * argv);
 int sti(char * n);
+void input(std::istream &in, int *m, size_t len);
+size_t num_col_lsr(int * arr, size_t a, size_t b);
 
 int main(int argc, char ** argv)
 {
-    if(argc<4)
-    {
-      std::cerr<<"Not enough arguments \n";
-      return 1;
-    }
-    if(argc>4)
-    {
-      std::cerr<<"Too many arguments \n";
-      return 1;
-    }
-    if(!isdig(argv[1]))
-    {
-      std::cerr<<"first parameter is not a number \n";
-      return 1;
-    }
-    int n = 0;
-    try
-    {
-      n = sti(argv[1]);
-    } catch(const std::overflow_error &e)
-    {
-      std::cerr<<e.what() << "\n";
-      return 1;
-    }
-    if(n!=1 || n!=2)
-    {
-      std::cerr<<"First parameter is out of range \n";
-      return 1;
-    }
+  if(argc<4)
+  {
+    std::cerr<<"Not enough arguments \n";
+    return 1;
+  }
+  if(argc>4)
+  {
+    std::cerr<<"Too many arguments \n";
+    return 1;
+  }
+  if(!isdig(argv[1]))
+  {
+    std::cerr<<"first parameter is not a number \n";
+    return 1;
+  }
+  int num = 0;
+  try
+  {
+    num = sti(argv[1]);
+  } catch(const std::overflow_error &e)
+  {
+    std::cerr<<e.what() << "\n";
+    return 1;
+  }
+  if(num!=1 || num!=2)
+  {
+    std::cerr<<"First parameter is out of range \n";
+    return 1;
+  }
 
-    size_t a,b;
-    std::ifstream in(argv[2]);
+  size_t a,b;
+  std::ifstream in(argv[2]);
+
+  if(!in.is_open())
+  {
+    std::cerr<<"Not correct file";
+    return 2;
+  }
     
-    if(!in.is_open())
+  if(in.peek() == std::ifstream::traits_type::eof())
+  {
+    std::cerr << "File is empty";
+    return 2;
+  }
+
+  if(!(in >> a >> b))
+  {
+    std::cerr <<"Not correct file";
+    return 2;
+  }
+  size_t len = a*b;
+  int arr2[10000] = {};
+  int *arr = new int [len];
+  std::ifstream in(argv[2]);
+  size_t m, n;
+  in >> m >> n;
+  int *matr = nullptr;
+  try
+  {
+    if(num == 1)
     {
-        std::cerr<<"Not correct file";
-        return 2;
+      input(in,arr2,len);
+      matr = arr2;
+      delete[] arr;
     }
-    
-    if(in.peek() == std::ifstream::traits_type::eof())
+    if(num == 2)
     {
-        std::cerr << "File is empty";
-        return 2;
+      input(in,arr,len);
+      matr = arr;
     }
-
-    if(!(in >> a >> b))
+    } catch(std::logic_error &e)
     {
-        std::cerr <<"Not correct file";
-        return 2;
+      std::cerr<<e.what()<<"\n";
+      return 2;
     }
-    size_t len = a*b;
-    int arr2[10000] = {};
-    int *arr = new int [len];
-    std::ifstream in(argv[2]);
-    size_t m, n;
-    in >> m >> n;
-
-}
-
-
+  }
 bool isdig(char * argv)
 {
     size_t l = std::strlen(argv);
