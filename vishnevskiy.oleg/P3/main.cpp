5ddd40286763bc447e516123a5b75de204e1cd19
Oleg <n7o.nto@yandex.ru>
2025-11-15 23:59:25 +0300

    Task finished

--- vishnevskiy.oleg/P3/main.cpp
+++ vishnevskiy.oleg/P3/main.cpp
@@ -2,6 +2,18 @@
 #include <fstream>
 #include <string>
 
+int localMax(int * mt, int n, int m){
+  int res = 0;
+  for (size_t i = 0; i < n; ++i){
+    for (size_t j = 1; j < m-1; ++j){
+      if (mt[i*n+(j-1)]<mt[i*n+j] && mt[i*n+j]>mt[i*n+(j+1)]){
+        res++;
+      }
+    }
+  }
+  return res;
+}
+
 bool bottomTri(int * mt, int n){
   int zNum = 0, zRow = 0, f = 1;
   for (size_t i = 0; i < n; ++i){
@@ -42,14 +54,34 @@ int main(){
     for (size_t i = 0; i < n*m; ++i){
       input >> matrix[i];
     }
-    std::cout<<bottomTri(matrix,n)<<'\n';
+    std::ofstream out;
+    out.open(outputName);
+    if (out.is_open()){
+      if (n<m){
+        out << bottomTri(matrix,n) << '\n';
+      }
+      else{
+        out<< bottomTri(matrix,m) << '\n';
+      }
+      out<<localMax(matrix,n,m) << '\n';
+    }
   }
   else if (num == 2){
     int * matrix = new int[n*m];
     for (size_t i = 0; i < n*m; ++i){
       input >> matrix[i];
     }
-    std::cout<<bottomTri(matrix,n)<<'\n';
+    std::ofstream out;
+    out.open(outputName);
+    if (out.is_open()){
+      if (n<m){
+        out << bottomTri(matrix,n) << '\n';
+      }
+      else{
+        out<< bottomTri(matrix,m) << '\n';
+      }
+      out<<localMax(matrix,n,m) << '\n';
+    }
   }
   else{
     std::cerr<<"Invalid number"<<'\n';
