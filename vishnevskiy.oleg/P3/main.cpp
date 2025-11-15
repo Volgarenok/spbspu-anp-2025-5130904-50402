4cb0a4823d8d416751cea025ed7e88c8126c8f40
Oleg <n7o.nto@yandex.ru>
2025-11-15 23:00:38 +0300

    Input

--- vishnevskiy.oleg/P3/main.cpp
+++ vishnevskiy.oleg/P3/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(){
-  return 0;
+  int num = 0, n = 0, m = 0;
+  std::string inputName = "blank", outputName = "blank";
+  std::cin >> num >> inputName >> outputName;
+  std::fstream input;
+  input.open(inputName);
+  if (!input){
+    std::cerr<<"File not found"<<'\n';
+    return 1;
+  }
+  input >> n >> m;
+  if (num == 1){
+    int matrix[n*m];
+    for (size_t i = 0; i < n*m; ++i){
+      input >> matrix[i];
+    }
+  }
+  else if (num == 2){
+    int * matrix = new int[n*m];
+    for (size_t i = 0; i < n*m; ++i){
+      input >> matrix[i];
+    }
+  }
+  else{
+    std::cerr<<"Invalid number"<<'\n';
+    return 1;
+  }
 }
