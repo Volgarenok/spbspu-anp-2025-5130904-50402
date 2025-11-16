34887f3c53682b10669ba65e5da5956200b998ef
EGGorkaa <egor_novikov20004@mail.ru>
2025-11-16 18:23:49 +0300

    some fix...

--- novikov.egor/P3/main.cpp
+++ novikov.egor/P3/main.cpp
@@ -267,7 +267,8 @@ MatrixD get_matrix_dynamic(const char* input_file)
 
   int rows = 0, cols = 0;
   input >> rows >> cols;
-  int len = rows * cols;
+  int len = 0;
+  len = rows * cols;
   if (rows < 3 || cols < 3) {
     std::cerr << "matrix is too small...\n";
     throw std::logic_error("cant open");
