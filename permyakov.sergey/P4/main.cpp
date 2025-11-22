f267940831ccf1ffd29b64ee34f5467431e27a94
Stave143 <staveplay@mail.ru>
2025-11-22 12:45:03 +0300

    add cinStr for lines

--- permyakov.sergey/P4/main.cpp
+++ permyakov.sergey/P4/main.cpp
@@ -2,18 +2,17 @@
 
 namespace permyakov
 {
-  void addSymToStr(char * line, const char sym, size_t & size);
-  void cinStr(char * line, size_t & size);
+  void addSymToStr(char * & line, const char sym, size_t & size);
+  void cinStr(char * & line, size_t & size);
 }
 
 int main()
 {
-  return 0;
   char * line1 = nullptr;
   size_t size1 = 0;
   char * line2 = nullptr;
   size_t size2 = 0;
-  
+
   try{
     line1 = new char[size1];
   } catch (std::bad_alloc()) {
@@ -29,11 +28,25 @@ int main()
     return 2;
   }
 
+  namespace per = permyakov;
+
+  try{
+    per::cinStr(line1, size1);
+    per::cinStr(line2, size2);
+  } catch (...) {
+    std::cerr << "Failure to read str\n";
+    delete[] line1;
+    delete[] line2;
+    return 1;
+  }
+  std::cout << line1 << ' ' << size1 << '\n';
+  std::cout << line2 << ' ' << size2 << '\n';
+
   delete[] line1;
   delete[] line2;
 }
 
-void permyakov::addSymToStr(char * line, const char sym, size_t & size)
+void permyakov::addSymToStr(char * & line, const char sym, size_t & size)
 {
   char * newLine = new char[size + 1];
   for(size_t i = 0; i < size; ++i){
@@ -45,10 +58,12 @@ void permyakov::addSymToStr(char * line, const char sym, size_t & size)
   size++;
 }
 
-void permyakov::cinStr(char * line, size_t & size)
+void permyakov::cinStr(char * & line, size_t & size)
 {
   char sym = ' ';
-  while (std::cin >> sym){
+  std::cin.get(sym);
+  while (sym != '\n'){
     addSymToStr(line, sym, size);
+    std::cin.get(sym);
   }
 }
