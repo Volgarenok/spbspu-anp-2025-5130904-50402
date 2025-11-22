31d1510eb0b8a00e01ff1ff933f29de265d9b79a
Stave143 <staveplay@mail.ru>
2025-11-22 12:54:29 +0300

    do UNI-TWO. fix CG-14

--- permyakov.sergey/P4/main.cpp
+++ permyakov.sergey/P4/main.cpp
@@ -16,14 +16,14 @@ int main()
   char * line2 = nullptr;
   size_t size2 = 0;
 
-  try{
+  try {
     line1 = new char[size1 + 1];
   } catch (std::bad_alloc()) {
     std::cerr << "Failure to allocate memory\n";
     return 2;
   }
 
-  try{
+  try {
     line2 = new char[size2 + 1];
   } catch (std::bad_alloc()) {
     std::cerr << "Failure to allocate memory\n";
@@ -36,7 +36,7 @@ int main()
   line1[0] = '\0';
   line2[0] = '\0';
 
-  try{
+  try {
     per::cinStr(line1, size1);
     per::cinStr(line2, size2);
   } catch (...) {
@@ -49,13 +49,13 @@ int main()
   char * result1 = nullptr;
   size_t resSize1 = 0;
 
-  for (size_t i = 0; i < size1; ++i){
-    if (isalpha(line1[i])){
+  for (size_t i = 0; i < size1; ++i) {
+    if (isalpha(line1[i])) {
       resSize1++;
     }
   }
-  for (size_t i = 0; i < size2; ++i){
-    if (isalpha(line2[i])){
+  for (size_t i = 0; i < size2; ++i) {
+    if (isalpha(line2[i])) {
       resSize1++;
     }
   }
@@ -63,7 +63,7 @@ int main()
   char * result2 = nullptr;
   size_t resSize2 = size1 + size2;
 
-  try{
+  try {
     result1 = new char[resSize1 + 1];
   } catch (std::bad_alloc()) {
     delete[] line1;
@@ -72,7 +72,7 @@ int main()
     return 2;
   }
 
-  try{
+  try {
     result2 = new char[resSize2 + 1];
   } catch (std::bad_alloc()) {
     std::cerr << "Failure to allocate memory\n";
@@ -100,7 +100,7 @@ int main()
 void permyakov::addSymToStr(char * & line, const char sym, size_t & size)
 {
   char * newLine = new char[size + 2];
-  for (size_t i = 0; i < size; ++i){
+  for (size_t i = 0; i < size; ++i) {
     newLine[i] = line[i];
   }
   newLine[size] = sym;
@@ -114,7 +114,7 @@ void permyakov::cinStr(char * & line, size_t & size)
 {
   char sym = ' ';
   std::cin.get(sym);
-  while (sym != '\n'){
+  while (sym != '\n') {
     addSymToStr(line, sym, size);
     std::cin.get(sym);
   }
@@ -123,23 +123,23 @@ void permyakov::cinStr(char * & line, size_t & size)
 void permyakov::LAT_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine, const size_t size)
 {
   size_t istr = 0;
-  for (size_t i = 0; i < size1; ++i){
-    if (isalpha(line1[i])){
+  for (size_t i = 0; i < size1; ++i) {
+    if (isalpha(line1[i])) {
       newLine[istr] = line1[i];
       istr++;
     }
   }
-  for (size_t i = 0; i < size2; ++i){
-    if (isalpha(line2[i])){
+  for (size_t i = 0; i < size2; ++i) {
+    if (isalpha(line2[i])) {
       newLine[istr] = line2[i];
       istr++;
     }
   }
 
   char swch = ' ';
-  for (size_t i = size - 1; i > 0; --i){
-    for (size_t j = 0; j < i; ++j){
-      if (newLine[j] > newLine[j + 1]){
+  for (size_t i = size - 1; i > 0; --i) {
+    for (size_t j = 0; j < i; ++j) {
+      if (newLine[j] > newLine[j + 1]) {
 	swch = newLine[j];
 	newLine[j] = newLine[j + 1];
 	newLine[j + 1] = swch;
@@ -150,9 +150,18 @@ void permyakov::LAT_TWO(const char * line1, const size_t size1, const char * lin
 
 void permyakov::UNI_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine)
 {
-  (void)line1;
-  (void)size1;
-  (void)line2;
-  (void)size2;
-  (void)newLine;
+  size_t minSize = size1 < size2 ? size1 : size2;
+  for (size_t i = 0; i < minSize; ++i) {
+    newLine[i * 2] = line1[i];
+  }
+  for (size_t i = 0; i < minSize; ++i) {
+    newLine[i * 2 + 1] = line2[i];
+  }
+
+  const char * maxLine = size1 > size2 ? line1 : line2;
+  size_t maxSize = size1 > size2 ? size1 : size2;
+
+  for (size_t i = minSize; i < maxSize; ++i) {
+    newLine[minSize + i] = maxLine[i];
+  }
 }
