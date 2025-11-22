b47aebb9bdfd85df16fe124134c1b3240deb429e
Stave143 <staveplay@mail.ru>
2025-11-22 12:46:51 +0300

    declorate function UNI-TWO and LAT-TWO. Add result init and out in main

--- permyakov.sergey/P4/main.cpp
+++ permyakov.sergey/P4/main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+#include<cctype>
 
 namespace permyakov
 {
   void addSymToStr(char * & line, const char sym, size_t & size);
   void cinStr(char * & line, size_t & size);
+  void LAT_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine);
+  void UNI_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine);
 }
 
 int main()
@@ -39,9 +42,51 @@ int main()
     delete[] line2;
     return 1;
   }
-  std::cout << line1 << ' ' << size1 << '\n';
-  std::cout << line2 << ' ' << size2 << '\n';
 
+  char * result1 = nullptr;
+  size_t resSize1 = 0;
+
+  for (size_t i = 0; i < size1; ++i){
+    if (isalpha(line1[i])){
+      resSize1++;
+    }
+  }
+  for (size_t i = 0; i < size2; ++i){
+    if (isalpha(line2[i])){
+      resSize1++;
+    }
+  }
+
+  char * result2 = nullptr;
+  size_t resSize2 = size1 + size2;
+
+  try{
+    result1 = new char[resSize1];
+  } catch (std::bad_alloc()) {
+    delete[] line1;
+    delete[] line2;
+    std::cerr << "Failure to allocate memory\n";
+    return 2;
+  }
+
+  try{
+    result2 = new char[resSize2];
+  } catch (std::bad_alloc()) {
+    std::cerr << "Failure to allocate memory\n";
+    delete[] line1;
+    delete[] line2;
+    delete[] result1;
+    return 2;
+  }
+
+  per::LAT_TWO(line1, size1, line2, size2, result1);
+  per::UNI_TWO(line1, size1, line2, size2, result2);
+
+  std::cout << result1 << '\n';
+  std::cout << result2 << '\n';
+
+  delete[] result1;
+  delete[] result2;
   delete[] line1;
   delete[] line2;
 }
@@ -49,7 +94,7 @@ int main()
 void permyakov::addSymToStr(char * & line, const char sym, size_t & size)
 {
   char * newLine = new char[size + 1];
-  for(size_t i = 0; i < size; ++i){
+  for (size_t i = 0; i < size; ++i){
     newLine[i] = line[i];
   }
   newLine[size] = sym;
@@ -66,4 +111,22 @@ void permyakov::cinStr(char * & line, size_t & size)
     addSymToStr(line, sym, size);
     std::cin.get(sym);
   }
+}
+
+void permyakov::LAT_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine)
+{
+  (void)line1;
+  (void)size1;
+  (void)line2;
+  (void)size2;
+  (void)newLine;
+}
+
+void permyakov::UNI_TWO(const char * line1, const size_t size1, const char * line2, const size_t size2, char * newLine)
+{
+  (void)line1;
+  (void)size1;
+  (void)line2;
+  (void)size2;
+  (void)newLine;
 }
