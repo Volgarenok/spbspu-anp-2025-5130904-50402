ca0b153d2d3a2fd8d5436dd42c12030af78fdf3e
hihikomori <ghhhhhnika@gmail.com>
2025-12-25 19:47:19 +0300

    feat: swap all float to double, created rect implementation

--- strelnikov.matvey/P5/main.cpp
+++ strelnikov.matvey/P5/main.cpp
@@ -1,30 +1,30 @@
 #include <cstdlib>
-
+#include <iostream>
 namespace strelnikov {
   struct point_t {
-    float x_, y_;
+    double x_, y_;
   };
   struct rectangle_t {
-    float width_, height_;
+    double width_, height_;
     point_t c_;
   };
   class IShape {
   public:
-    virtual float getArea() = 0;
+    virtual double getArea() = 0;
     virtual rectangle_t getFrameRect() = 0;
     virtual void move(point_t) = 0;
-    virtual void move(float, float) = 0;
-    virtual void scale(float) = 0;
+    virtual void move(double, double) = 0;
+    virtual void scale(double) = 0;
     virtual ~IShape() = default;
   };
   class Rectangle : public IShape {
   public:
-    Rectangle(float, float, point_t);
-    float getArea() override;
+    Rectangle(double, double, point_t);
+    double getArea() override;
     rectangle_t getFrameRect() override;
     void move(point_t) override;
-    void move(float, float) override;
-    void scale(float) override;
+    void move(double, double) override;
+    void scale(double) override;
     ~Rectangle() override = default;
 
   private:
@@ -34,34 +34,64 @@ namespace strelnikov {
   class Polygon : public IShape {
   public:
     Polygon(point_t*, size_t);
-    float getArea() override;
+    double getArea() override;
     rectangle_t getFrameRect() override;
     void move(point_t) override;
-    void move(float, float) override;
-    void scale(float) override;
+    void move(double, double) override;
+    void scale(double) override;
     ~Polygon() override = default;
 
   private:
     point_t* data_;
     size_t size_;
+    point_t c_;
   };
 
   class Circle : public IShape {
   public:
-    Circle(point_t, float);
-    float getArea() override;
+    Circle(point_t, double);
+    double getArea() override;
     rectangle_t getFrameRect() override;
     void move(point_t) override;
-    void move(float, float) override;
-    void scale(float) override;
+    void move(double, double) override;
+    void scale(double) override;
     ~Circle() override = default;
 
   private:
     point_t cen_;
-    float rad_;
+    double rad_;
   };
+}; // namespace strelnikov
 
-};
+strelnikov::Rectangle::Rectangle(double wdth, double hght, point_t cntr) : rec_{wdth, hght, cntr}
+{}
+
+double strelnikov::Rectangle::getArea()
+{
+  return rec_.height_ * rec_.width_;
+}
+
+strelnikov::rectangle_t strelnikov::Rectangle::getFrameRect()
+{
+  return rec_;
+}
+
+void strelnikov::Rectangle::move(strelnikov::point_t p)
+{
+  rec_.c_.y_ += p.y_;
+  rec_.c_.x_ += p.x_;
+}
+
+void strelnikov::Rectangle::move(double x, double y)
+{
+  strelnikov::Rectangle::move({x,y});
+}
+
+void strelnikov::Rectangle::scale(double k)
+{
+  strelnikov::Rectangle::rec_.c_.x_ *= k;
+  strelnikov::Rectangle::rec_.c_.y_ *= k;
+}
 
 int main()
 {}
