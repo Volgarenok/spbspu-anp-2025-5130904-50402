2282a9375ccc9b047eb2133851aa3b319f3d4346
hihikomori <ghhhhhnika@gmail.com>
2025-12-25 22:18:02 +0300

    feat: set up funcs for calculating center of poly

--- strelnikov.matvey/P5/main.cpp
+++ strelnikov.matvey/P5/main.cpp
@@ -61,7 +61,12 @@ namespace strelnikov {
     point_t cen_;
     double rad_;
   };
-}; // namespace strelnikov
+
+  double get_tr_poly_S(point_t a, point_t b, point_t c);
+  point_t get_tr_poly_C(point_t a, point_t b, point_t c);
+  point_t get_poly_C();
+
+};
 
 strelnikov::Rectangle::Rectangle(double wdth, double hght, point_t cntr) : rec_{wdth, hght, cntr}
 {}
@@ -84,7 +89,7 @@ void strelnikov::Rectangle::move(strelnikov::point_t p)
 
 void strelnikov::Rectangle::move(double x, double y)
 {
-  strelnikov::Rectangle::move({x,y});
+  strelnikov::Rectangle::move({x, y});
 }
 
 void strelnikov::Rectangle::scale(double k)
@@ -92,6 +97,43 @@ void strelnikov::Rectangle::scale(double k)
   strelnikov::Rectangle::rec_.c_.x_ *= k;
   strelnikov::Rectangle::rec_.c_.y_ *= k;
 }
+
+double strelnikov::get_tr_poly_S(strelnikov::point_t a, strelnikov::point_t b, strelnikov::point_t c)
+{
+  double res = a.x_ * (b.y_ - c.y_) + b.x_ * (c.y_ - a.y_) + c.x_ * (a.y_ - b.y_);
+  if (res < 0) {
+    res *= -1;
+  }
+  res *= 0.5;
+  return res;
+}
+
+strelnikov::point_t strelnikov::get_tr_poly_C(strelnikov::point_t a, strelnikov::point_t b, strelnikov::point_t c)
+{
+  double x = (a.x_ + b.x_ + c.x_) / 3.0;
+  double y = (a.y_ + b.y_ + c.y_) / 3.0;
+  return {x, y};
+}
+
+strelnikov::point_t strelnikov::get_poly_C(const strelnikov::point_t* data, size_t k)
+{
+  double sum_s = 0;
+  double sum_x = 0;
+  double sum_y = 0;
+  for (size_t i = 1; i < (k - 1); ++i) {
+    double cur_sum = strelnikov::get_tr_poly_S(data[i], data[i + 1], data[0]);
+    sum_s += cur_sum;
+    sum_x += ((strelnikov::get_tr_poly_C(data[i], data[i + 1], data[0])).x_ * cur_sum);
+    sum_y += ((strelnikov::get_tr_poly_C(data[i], data[i + 1], data[0])).y_ * cur_sum);
+  }
+
+  if (sum_s == 0.0) {
+    throw std::logic_error("Bad poly");
+  }
+  return {(sum_x / sum_s), (sum_y / sum_s)};
+}
+
+
 
 int main()
 {}
