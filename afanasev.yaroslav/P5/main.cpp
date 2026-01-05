a3e1e81ff73a808688c18e12d0ea89fa0c2e4558
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-01-05 20:48:19 +0500

    Make interface

--- afanasev.yaroslav/P5/main.cpp
+++ afanasev.yaroslav/P5/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+namespace afanasev
+{
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+
+  struct rectangle_t
+  {
+    double width, height;
+    point_t pos;
+  };
+
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+
+    virtual double getArea() const = 0;
+
+    virtual rectangle_t getFrameRect() const = 0;
+
+    virtual void move(const point_t& point) = 0;
+    virtual void move(double x, double y) = 0;
+
+    virtual void scale(double coefficient) = 0;
+  };
+}
+
+
+
 int main()
 {
+  using afanasev::point_t;
+  using afanasev::rectangle_t;
+  using afanasev::Shape;
+
   return 0;
 }
