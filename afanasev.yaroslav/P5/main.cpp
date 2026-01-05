b4e4b276b206107b5fdc44cccbdbd213ee859f4f
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-01-05 21:08:58 +0500

    Make rectangle getArea

--- afanasev.yaroslav/P5/main.cpp
+++ afanasev.yaroslav/P5/main.cpp
@@ -20,6 +20,8 @@ namespace afanasev
     virtual ~Shape() = default;
 
     virtual double getArea() const = 0;
+/*
+    
 
     virtual rectangle_t getFrameRect() const = 0;
 
@@ -27,7 +29,41 @@ namespace afanasev
     virtual void move(double x, double y) = 0;
 
     virtual void scale(double coefficient) = 0;
+    */
   };
+
+  class Rectangle : public Shape
+  {
+    double width_;
+    double height_;
+    point_t center_;
+
+  public:
+    Rectangle(double w, double h, point_t pos)
+    {
+      if (w <= 0 || h <= 0)
+      {
+        throw std::invalid_argument("scale must be > 0");
+      }
+      width_ = w;
+      height_ = h;
+      center_ = pos;
+    }
+
+    double getArea() const override
+    {
+      return width_ * height_;
+    }
+
+
+
+  };
+
+
+  void printShapesInfo()
+  {
+    std::cout << "f" << '\n';
+  }
 }
 
 
@@ -36,7 +72,22 @@ int main()
 {
   using afanasev::point_t;
   using afanasev::rectangle_t;
-  using afanasev::Shape;
+  using afanasev::Rectangle;
 
-  return 0;
+
+  try
+  {
+    Rectangle * rect1 = new Rectangle(4, 3, {8, 6});
+
+    std::cout << rect1 -> getArea() << '\n';
+
+    delete rect1;
+
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Ошибка: " << e.what() << std::endl;
+    return 1;
+  }
 }
