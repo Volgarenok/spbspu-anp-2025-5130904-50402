294bc19ecff3e2a1d5b436e08e29431a83888722
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-01-06 15:33:02 +0500

    Make user intarface

--- afanasev.yaroslav/P5/main.cpp
+++ afanasev.yaroslav/P5/main.cpp
@@ -24,20 +24,21 @@ namespace afanasev
     virtual rectangle_t getFrameRect() const = 0;
 
     virtual void move(const point_t& point) = 0;
-    virtual void move(const double dx, const double dy) = 0;
+    virtual void move(double dx, double dy) = 0;
 
-    virtual void scale(const double k) = 0;
+    virtual void scale(double k) = 0;
   };
 
   class Rectangle : public Shape
   {
-  public:
     double width_;
     double height_;
     point_t pos_;
     point_t center_;
 
-    Rectangle(const double w, const double h, const point_t pos)
+  public:
+
+    Rectangle(double w, double h, point_t pos)
     {
       if (w <= 0 || h <= 0)
       {
@@ -66,13 +67,13 @@ namespace afanasev
       center_ = point;
     }
 
-    void move(const double dx, const double dy) override
+    void move(double dx, double dy) override
     {
       center_.x += dx;
       center_.y += dy;
     }
 
-    void scale(const double k) override
+    void scale(double k) override
     {
       if (k <= 0)
       {
@@ -92,46 +93,149 @@ namespace afanasev
   };
 
 
-  void printShapesInfo()
+  void printShapesInfo(Shape ** const shapes, size_t cnt_shapes)
   {
-    std::cout << "f" << '\n';
+    std::cout << "Существующие фигуры:" << '\n';
+
+    size_t s_all = 0;
+
+    for (size_t i = 0; i < cnt_shapes; i++)
+    {
+      // площадь
+      s_all += (shapes[i] -> getArea());
+
+      std::cout << i << ") S = " << shapes[i] -> getArea();
+
+      // Размер
+      std::cout << "; Размер ограничивающего прямоугольника: {h = " << shapes[i] -> getFrameRect().height;
+      std::cout << ", w = " << shapes[i] -> getFrameRect().width << '}';
+
+      // Положение фигуры
+      std::cout << "; Координаты фигуры: {" << shapes[i] -> getFrameRect().pos.x;
+      std::cout << ", " << shapes[i] -> getFrameRect().pos.y << '}';
+
+      std::cout << '\n';
+    }
+    
+    std::cout << "Общая площадь: " << s_all;
+    
+    std::cout << '\n';
   }
 }
-
-
 
 int main()
 {
   using afanasev::point_t;
   using afanasev::rectangle_t;
+  using afanasev::Shape;
   using afanasev::Rectangle;
+
+  size_t cnt_shapes = 3;
+  Shape ** shapes = nullptr;
+
+  try
+  {
+    shapes = new Shape * [cnt_shapes];
+  }
+  catch (const std::bad_alloc&)
+  {
+    std::cerr << "bad alloc" << std::endl;
+    return 1;
+  }
 
 
   try
   {
-    Rectangle * rect1 = new Rectangle(2, 2, {2, 2});
-    std::cout << "прямоугольник 2 на 2:" << '\n';
-    std::cout << "площадь:" << '\n';
-    std::cout << rect1 -> getArea() << '\n';
+    shapes[0] = new Rectangle(2, 2, {2, 2});
+    shapes[1] = new Rectangle(3, 4, {5, 4});
+    shapes[2] = new Rectangle(3, 6, {11, 12});
 
-    std::cout << "положение до увеличения:" << '\n';
-    std::cout << rect1 -> getFrameRect().pos.x << '\n';
-    std::cout << rect1 -> getFrameRect().pos.y << '\n';
+    int n = 0;
+    size_t shape = 0;
 
-    rect1 -> move(-2, -2);
+    while (n != -1)
+    {
+      printShapesInfo(shapes, cnt_shapes);
 
-    rect1 -> scale(2);
-    
-    std::cout << "размер после увеличения:" << '\n';
-    std::cout << rect1 -> height_ << '\n';
-    std::cout << rect1 -> width_ << '\n';
+      std::cout << "\nВведите номер фигуры:\n";
 
-    std::cout << "положение после увеличения:" << '\n';
-    std::cout << rect1 -> getFrameRect().pos.x << '\n';
-    std::cout << rect1 -> getFrameRect().pos.y << '\n';
+      std::cin >> shape;
+      if (!std::cin)
+      {
+        throw std::logic_error("input error");
+      }
+      if (shape >= cnt_shapes)
+      {
+        std::cout << "Нет фигуры с таким номером" << '\n';
+      }
+      else
+      {
+        std::cout << "\nДействия:\n";
+        std::cout << "0) переместить центр масштабирования на x и y (move(x, y)):" << '\n';
+        std::cout << "1) сместить центр масштабирования на dx и dy (move(dx, dy)):" << '\n';
+        std::cout << "2) масштабировать на коофицент k относительно центра масштабирования (scale(k)):" << '\n';
+        std::cout << "введите номер нужного действия" << '\n';
+
+        std::cin >> n;
+        if (!std::cin)
+        {
+          throw std::logic_error("input error");
+        }
+
+        if (!n)
+        {
+          std::cout << "\nВведите x и y точки для перемещения центра:\n";
+          double x = 0;
+          double y = 0;
+          std::cin >> x >> y;
+
+          if (!std::cin)
+          {
+            throw std::logic_error("input error");
+          }
+          shapes[shape] -> move({x, y});
+          std::cout << "Центр перемещён в {" << x << ", " << y << "}\n";
+        }
+        else if (n == 1)
+        {
+          std::cout << "\nВведите dx и dy точки для смещения центра:\n";
+          double x = 0;
+          double y = 0;
+          std::cin >> x >> y;
+          if (!std::cin)
+          {
+            throw std::logic_error("input error");
+          }
+          shapes[shape] -> move(x, y);
+          std::cout << "Центр смещён на {" << x << ", " << y << "}\n";
+        }
+        else if (n == 2)
+        {
+          std::cout << "\nВведите k для масштабирования относительно центра:\n";
+          double k = 0;
+          std::cin >> k;
+          if (!std::cin)
+          {
+            throw std::logic_error("input error");
+          }
+          shapes[shape] -> scale(k);
+          std::cout << "Масштаб изменён в " << k << " раз" << '\n';
+        }
+        else
+        {
+          std::cout << "несуществующее действие" << '\n';
+        }
+      }
+
+      std::cout << '\n';
+    }
 
 
-    delete rect1;
+    for (size_t i = 0; i < cnt_shapes; i++)
+    {
+      delete shapes[i];
+    }
+    delete[] shapes;
 
     return 0;
   }
