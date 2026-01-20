70f75e6edf36a3da9c73cf368fd54c39a99d186a
Jaroslaw-super1 <Jarafan3@gmail.com>
2026-01-20 17:47:54 +0500

    Make fix

--- afanasev.yaroslav/P5/main.cpp
+++ afanasev.yaroslav/P5/main.cpp
@@ -7,10 +7,7 @@ namespace afanasev
     double x;
     double y;
 
-    bool operator==(const point_t& other) const
-    {
-      return x == other.x && y == other.y;
-    }
+    bool operator==(const point_t& other) const noexcept;
   };
 
   struct rectangle_t
@@ -24,202 +21,81 @@ namespace afanasev
   public:
     virtual ~Shape() = default;
 
-    virtual double getArea() const = 0;
+    virtual double getArea() const noexcept = 0;
 
-    virtual rectangle_t getFrameRect() const = 0;
+    virtual rectangle_t getFrameRect() const noexcept = 0;
 
-    virtual void move(const point_t& point) = 0;
-    virtual void move(double dx, double dy) = 0;
+    virtual void move(const point_t& point) noexcept = 0;
+    virtual void move(double dx, double dy) noexcept = 0;
 
     virtual void scale(double k) = 0;
   };
 
-  class Rectangle : public Shape
+  class Rectangle final: public Shape
   {
+  public:
+    Rectangle(double w, double h, const point_t & pos);
+
+    double getArea() const noexcept override;
+
+    rectangle_t getFrameRect() const noexcept override;
+
+    void move(const point_t & point) noexcept override;
+
+    void move(double dx, double dy) noexcept override;
+
+    void scale(double k) override;
+
+  private:
     double width_;
     double height_;
     point_t pos_;
     point_t center_;
-
-  public:
-
-    Rectangle(double w, double h, point_t pos)
-    {
-      if (w <= 0 || h <= 0)
-      {
-        throw std::invalid_argument("scale must be > 0");
-      }
-      width_ = w;
-      height_ = h;
-
-      center_ = pos;
-
-      pos_ = pos;
-    }
-
-    double getArea() const override
-    {
-      return width_ * height_;
-    }
-
-    rectangle_t getFrameRect() const override
-    {
-      return {width_, height_, pos_};
-    }
-
-    void move(const point_t& point) override
-    {
-      center_ = point;
-    }
-
-    void move(double dx, double dy) override
-    {
-      center_.x += dx;
-      center_.y += dy;
-    }
-
-    void scale(double k) override
-    {
-      if (k <= 0)
-      {
-        throw std::invalid_argument("coefficient must be > 0");
-      }
-
-      width_ *= k;
-      height_ *= k;
-
-      double vx = pos_.x - center_.x;
-      double vy = pos_.y - center_.y;
-
-      pos_.x += -vx + vx * k;
-      pos_.y += -vy + vy * k;
-    }
   };
 
-  class Circle : public Shape
+  class Circle final: public Shape
   {
-    double radius;
+  public:
+    Circle(double r, const point_t & pos);
+
+    double getArea() const noexcept override;
+
+    rectangle_t getFrameRect() const noexcept override;
+
+    void move(const point_t & point) noexcept override;
+
+    void move(double dx, double dy) noexcept override;
+
+    void scale(double k) override;
+
+  private:
+    double radius_;
     point_t pos_;
     point_t center_;
-
-  public:
-
-    Circle(double r, point_t pos)
-    {
-      if (r <= 0)
-      {
-        throw std::invalid_argument("radius must be > 0");
-      }
-      radius = r;
-      center_ = pos;
-      pos_ = pos;
-    }
-
-    double getArea() const override
-    {
-      return radius * radius * 3.1415;
-    }
-
-    rectangle_t getFrameRect() const override
-    {
-      return {radius * 2, radius * 2, pos_};
-    }
-
-    void move(const point_t& point) override
-    {
-      center_ = point;
-    }
-
-    void move(double dx, double dy) override
-    {
-      center_.x += dx;
-      center_.y += dy;
-    }
-
-    void scale(double k) override
-    {
-      if (k <= 0)
-      {
-        throw std::invalid_argument("coefficient must be > 0");
-      }
-
-      radius *= k;
-
-      double vx = pos_.x - center_.x;
-      double vy = pos_.y - center_.y;
-
-      pos_.x += -vx + vx * k;
-      pos_.y += -vy + vy * k;
-    }
   };
 
-  class Rubber : public Shape
+  class Rubber final: public Shape
   {
-    Circle c_min;
-    Circle c_max;
-
-    point_t center_;
-
   public:
+    Rubber(double r1, double r2, const point_t & pos1, const point_t & pos2);
 
-    Rubber(double r1, double r2, point_t pos1, point_t pos2):
-    c_min(r1, pos1),
-    c_max(r2, pos2)
-    {
-      if (r1 <= 0 || r2 <= 0 || r2 <= r1 || pos1 == pos2)
-      {
-        throw std::invalid_argument("incorrect input");
-      }
+    double getArea() const noexcept override;
 
-      double dx = pos1.x - pos2.x;
-      double dy = pos1.y - pos2.y;
-      double distance_squared = dx * dx + dy * dy;
+    rectangle_t getFrameRect() const noexcept override;
 
-      if (distance_squared >= (r2 - r1) * (r2 - r1))
-      {
-        throw std::invalid_argument("circle is collision");
-      }
+    void move(const point_t & point) noexcept override;
 
-      center_ = pos1;
-    }
+    void move(double dx, double dy) noexcept override;
 
-    double getArea() const override
-    {
-      return c_max.getArea() - c_min.getArea();
-    }
+    void scale(double k) override;
 
-    rectangle_t getFrameRect() const override
-    {
-      return c_max.getFrameRect();
-    }
-
-    void move(const point_t& point) override
-    {
-      center_ = point;
-    }
-
-    void move(double dx, double dy) override
-    {
-      center_.x += dx;
-      center_.y += dy;
-    }
-
-    void scale(double k) override
-    {
-      if (k <= 0)
-      {
-        throw std::invalid_argument("coefficient must be > 0");
-      }
-
-      c_min.move(center_);
-      c_max.move(center_);
-
-      c_min.scale(k);
-      c_max.scale(k);
-    }
+  private:
+    Circle c_min_;
+    Circle c_max_;
+    point_t center_;
   };
 
-  void printShapesInfo(Shape ** const shapes, size_t cnt_shapes)
+  void printShapesInfo(Shape const * const * const shapes, size_t cnt_shapes)
   {
     std::cout << "Существующие фигуры:" << '\n';
 
@@ -234,11 +110,11 @@ namespace afanasev
 
     for (size_t i = 0; i < cnt_shapes; i++)
     {
-      rectangle_t frame = shapes[i] -> getFrameRect();
+      rectangle_t frame = shapes[i]->getFrameRect();
 
-      s_all += (shapes[i] -> getArea());
+      s_all += (shapes[i]->getArea());
 
-      std::cout << i << ") S = " << shapes[i] -> getArea();
+      std::cout << i << ") S = " << shapes[i]->getArea();
 
       std::cout << "; Размер ограничивающего прямоугольника: {h = " << frame.height;
       std::cout << ", w = " << frame.width << '}';
@@ -263,22 +139,10 @@ namespace afanasev
       }
       else
       {
-        if (left < x_min)
-        {
-          x_min = left;
-        }
-        if (right > x_max)
-        {
-          x_max = right;
-        }
-        if (bottom < y_min)
-        {
-          y_min = bottom;
-        }
-        if (top > y_max)
-        {
-        y_max = top;
-        }
+        x_min = (left < x_min) ? left : x_min;
+        x_max = (right > x_max) ? right : x_max;
+        y_min = (bottom < y_min) ? bottom : y_min;
+        y_max = (top > y_max) ? top : y_max;
       }
     }
 
@@ -346,6 +210,7 @@ int main()
         std::cout << "0) переместить центр масштабирования на x и y (move(x, y)):" << '\n';
         std::cout << "1) сместить центр масштабирования на dx и dy (move(dx, dy)):" << '\n';
         std::cout << "2) масштабировать на коофицент k относительно центра масштабирования (scale(k)):" << '\n';
+        std::cout << "3) УЛЬТРА МЕГА ФУНКЦИЯ КОМБО 3000!!!! масштабировать на коофицент k относительно x и y:" << '\n';
         std::cout << "введите номер нужного действия" << '\n';
 
         std::cin >> n;
@@ -364,7 +229,7 @@ int main()
           {
             throw std::logic_error("input error");
           }
-          shapes[shape] -> move({x, y});
+          shapes[shape]->move({x, y});
           std::cout << "Центр перемещён в {" << x << ", " << y << "}\n";
         }
         else if (n == 1)
@@ -377,7 +242,7 @@ int main()
           {
             throw std::logic_error("input error");
           }
-          shapes[shape] -> move(x, y);
+          shapes[shape]->move(x, y);
           std::cout << "Центр смещён на {" << x << ", " << y << "}\n";
         }
         else if (n == 2)
@@ -389,8 +254,32 @@ int main()
           {
             throw std::logic_error("input error");
           }
-          shapes[shape] -> scale(k);
+          shapes[shape]->scale(k);
           std::cout << "Масштаб изменён в " << k << " раз" << '\n';
+        }
+        else if (n == 3)
+        {
+          std::cout << "\nВведите k для масштабирования относительно центра:\n";
+          double k = 0;
+          std::cin >> k;
+          if (!std::cin)
+          {
+            throw std::logic_error("input error");
+          }
+
+          std::cout << "\nВведите x и y центра для масштабирования:\n";
+          double x = 0;
+          double y = 0;
+          std::cin >> x >> y;
+          if (!std::cin)
+          {
+            throw std::logic_error("input error");
+          }
+
+          shapes[shape]->move({x, y});
+          shapes[shape]->scale(k);
+
+          std::cout << "Масштабирован в " << k << " раз, относительно {" << x << ", " << y << "}\n";
         }
         else
         {
@@ -421,4 +310,170 @@ int main()
   delete[] shapes;
 
   return 0;
+}
+
+
+// point_t
+bool afanasev::point_t::operator==(const point_t& other) const noexcept
+{
+  return x == other.x && y == other.y;
+}
+
+
+// Rectangle
+afanasev::Rectangle::Rectangle(double w, double h, const point_t & pos):
+  width_(w),
+  height_(h),
+  pos_(pos),
+  center_(pos)
+{
+  if (w <= 0 || h <= 0)
+  {
+    throw std::invalid_argument("scale must be > 0");
+  }
+}
+
+double afanasev::Rectangle::getArea() const noexcept
+{
+  return width_ * height_;
+}
+
+afanasev::rectangle_t afanasev::Rectangle::getFrameRect() const noexcept
+{
+  return {width_, height_, pos_};
+}
+
+void afanasev::Rectangle::move(const point_t & point) noexcept
+{
+  center_ = point;
+}
+
+void afanasev::Rectangle::move(double dx, double dy) noexcept
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+void afanasev::Rectangle::scale(double k)
+{
+  if (k <= 0)
+  {
+    throw std::invalid_argument("coefficient must be > 0");
+  }
+
+  width_ *= k;
+  height_ *= k;
+
+  double vx = pos_.x - center_.x;
+  double vy = pos_.y - center_.y;
+
+  pos_.x += -vx + vx * k;
+  pos_.y += -vy + vy * k;
+}
+
+
+// Circle
+afanasev::Circle::Circle(double r, const point_t & pos):
+  radius_(r),
+  pos_(pos),
+  center_(pos)
+{
+  if (r <= 0)
+  {
+    throw std::invalid_argument("radius must be > 0");
+  }
+}
+
+double afanasev::Circle::getArea() const noexcept
+{
+  return radius_ * radius_ * 3.1415;
+}
+
+afanasev::rectangle_t afanasev::Circle::getFrameRect() const noexcept
+{
+  return {radius_ * 2, radius_ * 2, pos_};
+}
+
+void afanasev::Circle::move(const point_t & point) noexcept
+{
+  center_ = point;
+}
+
+void afanasev::Circle::move(double dx, double dy) noexcept
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+void afanasev::Circle::scale(double k)
+{
+  if (k <= 0)
+  {
+    throw std::invalid_argument("coefficient must be > 0");
+  }
+
+  radius_ *= k;
+
+  double vx = pos_.x - center_.x;
+  double vy = pos_.y - center_.y;
+
+  pos_.x += -vx + vx * k;
+  pos_.y += -vy + vy * k;
+}
+
+
+// Rubber
+afanasev::Rubber::Rubber(double r1, double r2, const point_t & pos1, const point_t & pos2):
+  c_min_(r1, pos1),
+  c_max_(r2, pos2),
+  center_(pos1)
+{
+  if (r1 <= 0 || r2 <= 0 || r2 <= r1 || pos1 == pos2)
+  {
+    throw std::invalid_argument("incorrect input");
+  }
+
+  double dx = pos1.x - pos2.x;
+  double dy = pos1.y - pos2.y;
+  double distance_squared = dx * dx + dy * dy;
+
+  if (distance_squared >= (r2 - r1) * (r2 - r1))
+  {
+    throw std::invalid_argument("circle is collision");
+  }
+}
+
+double afanasev::Rubber::getArea() const noexcept
+{
+  return c_max_.getArea() - c_min_.getArea();
+}
+
+afanasev::rectangle_t afanasev::Rubber::getFrameRect() const noexcept
+{
+  return c_max_.getFrameRect();
+}
+
+void afanasev::Rubber::move(const point_t & point) noexcept
+{
+  center_ = point;
+}
+
+void afanasev::Rubber::move(double dx, double dy) noexcept
+{
+  center_.x += dx;
+  center_.y += dy;
+}
+
+void afanasev::Rubber::scale(double k)
+{
+  if (k <= 0)
+  {
+    throw std::invalid_argument("coefficient must be > 0");
+  }
+
+  c_min_.move(center_);
+  c_max_.move(center_);
+
+  c_min_.scale(k);
+  c_max_.scale(k);
 }
